c03679a7936a65bd270e6363fb134549301acc0c
Bertrand Dunogier <bertrand.dunogier@ez.no>
2019-12-16 12:33:43 +0100

    fixup! QueryResultsInjector specs

--- spec/eZ/ContentView/QueryResultsInjectorSpec.php
+++ spec/eZ/ContentView/QueryResultsInjectorSpec.php
@@ -2,21 +2,23 @@
 
 namespace spec\EzSystems\EzPlatformQueryFieldType\eZ\ContentView;
 
+use eZ\Publish\Core\MVC\Symfony\View\ContentView;
 use eZ\Publish\Core\MVC\Symfony\View\Event\FilterViewParametersEvent;
-use eZ\Publish\Core\MVC\Symfony\View\View;
 use eZ\Publish\Core\MVC\Symfony\View\ViewEvents;
+use eZ\Publish\Core\Repository\Values\Content\Content;
 use EzSystems\EzPlatformQueryFieldType\API\QueryFieldServiceInterface;
 use EzSystems\EzPlatformQueryFieldType\eZ\ContentView\QueryResultsInjector;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class QueryResultsInjectorSpec extends ObjectBehavior
 {
-    const FIELD_VIEW = 'line';
+    const FIELD_VIEW = 'content_query_field';
     const OTHER_VIEW = 'anything_else';
-    const VIEWS = ['field' => self::FIELD_VIEW, 'item' => 'field'];
+    const ITEM_VIEW = 'line';
+    const VIEWS = ['field' => self::FIELD_VIEW, 'item' => self::ITEM_VIEW];
+    const FIELD_DEFINITION_IDENTIFIER = 'query_field';
 
     function it_is_initializable()
     {
@@ -27,12 +29,14 @@ class QueryResultsInjectorSpec extends ObjectBehavior
         QueryFieldServiceInterface $queryFieldService,
         FilterViewParametersEvent $event,
         ParameterBagInterface $parameterBag,
-        View $view
+        ContentView $view
     )
     {
         $this->beConstructedWith($queryFieldService, self::VIEWS);
         $event->getView()->willReturn($view);
+        $view->getContent()->willReturn($this->createContentItem());
         $event->getParameterBag()->willReturn($parameterBag);
+        $event->getBuilderParameters()->willReturn(['queryFieldDefinitionIdentifier' => self::FIELD_DEFINITION_IDENTIFIER]);
     }
 
     function it_throws_an_InvalidArgumentException_if_no_item_view_is_provided(QueryFieldServiceInterface $queryFieldService)
@@ -57,11 +61,30 @@ class QueryResultsInjectorSpec extends ObjectBehavior
         $this->getSubscribedEvents()->shouldSubscribeTo(ViewEvents::FILTER_VIEW_PARAMETERS);
     }
 
-    function it_does_nothing_for_non_field_views(FilterViewParametersEvent $event, View $view)
+    function it_does_nothing_for_non_field_views(FilterViewParametersEvent $event, ContentView $view)
     {
         $view->getViewType()->willReturn(self::OTHER_VIEW);
         $this->injectQueryResults($event);
         $event->getParameterBag()->shouldNotHaveBeenCalled();
+    }
+
+    function it_adds_the_query_results_for_the_field_view(FilterViewParametersEvent $event, ParameterBagInterface $parameterBag, ContentView $view, QueryFieldServiceInterface $queryFieldService)
+    {
+        $view->getViewType()->willReturn(self::FIELD_VIEW);
+
+        $queryFieldService->loadContentItems(
+            $this->createContentItem(),
+            self::FIELD_DEFINITION_IDENTIFIER
+        )->willReturn($this->getResults());
+
+        $parameterBag->add(
+            [
+                'itemViewType' => self::ITEM_VIEW,
+                'items' => $this->getResults(),
+            ]
+        )->shouldBeCalled();
+
+        $this->injectQueryResults($event);
     }
 
     function getMatchers(): array
@@ -70,6 +93,19 @@ class QueryResultsInjectorSpec extends ObjectBehavior
             'subscribeTo' => function($return, $event) {
                 return is_array($return) && isset($return[$event]);
             }
+        ];
+    }
+
+    private function createContentItem(): Content
+    {
+        return new Content();
+    }
+
+    private function getResults(): array
+    {
+        return [
+            new Content(),
+            new Content(),
         ];
     }
 }
